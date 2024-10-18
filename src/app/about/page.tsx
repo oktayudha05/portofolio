f937929ba079cdd577b5d79b0b647b9b61a4c277
oktayudha05 <oktayudha05@gmail.com>
2024-10-18 21:02:46 +0700

    fix apos

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,11 +10,11 @@ export default function About() {
       <div className="pt-2 font-base">
         <div className="mb-10 text-base sm:text-lg">
           <p>
-            I'm an Information Technology student specializing in Backend
-            Development. Based in Magelang, I am passionate about building
-            scalable and efficient systems that solve real-world problems.
-            Currently, I'm working with technologies like Node.js, Express,
-            MongoDB, and NextJS to sharpen my skills.
+            {`I'm an Information Technology student specializing in Backend
+              Development. Based in Magelang, I am passionate about building
+              scalable and efficient systems that solve real-world problems.
+              Currently, I'm working with technologies like Node.js, Express,
+              MongoDB, and NextJS to sharpen my skills.`}
           </p>
         </div>
 
