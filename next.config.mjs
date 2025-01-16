4730484c9cae3f79e1f2355b854fb1a3767e0475
oktayudha05 <oktayudha05@gmail.com>
2025-01-16 22:52:45 +0700

    fix: red proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        outputStandalone: true, // Optional, hanya untuk standalone builds
+    },
+    serverRuntimeConfig: {
+        trustProxy: true, // Mengizinkan X-Forwarded-Proto untuk mendeteksi HTTPS
+    },
+};
 
-export default nextConfig
+export default nextConfig;
